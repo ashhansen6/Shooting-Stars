495b4ca18d5765ea17b06676c4336c4058acd70b
Ashton Hansen <ashhansen6@outlook.com>
2023-06-07 22:00:22 -0400

    A true initialization of the repository adding the basic files needed to build the simulator.

---
+++ conditions.R
@@ -0,0 +1,8 @@
+## Location
+location = "Richard Bong Recreational Park, WI"
+
+## Wind
+# Wind speed, miles per hour (mph)
+wind_speed = 10
+# Wind direction, degrees clockwise of North
+wind_dir = 120
---
+++ parameters.R
@@ -0,0 +1,19 @@
+## Rocket 
+# Name
+rkt_name = "ESTES HI-FLIER"
+# Weight in pounds (lbs)
+rkt_wt = 0.1625
+# Recovery system
+rkt_rcvr = "STREAMER"
+# Descent rate, feet per second (ft/s)
+rkt_dsc = 20
+
+## Engine
+# Name
+eng_name = "ESTES A8-3"
+# Thrust, pounds-force (lbf)
+eng_thrust = 1.1
+# Burn time, seconds (s)
+eng_t_burn = 3.5
+# Time to ejection, seconds (s)
+eng_t_eject = 6.5
