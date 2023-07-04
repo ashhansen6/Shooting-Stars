02f50566c03c5c7ef775c9a6f14d972acf2bda38
Ashton Hansen <ashhansen6@outlook.com>
2023-07-04 14:39:25 -0500

    Overhauled parameter system to store model information in lists, archived old parameters script.

---
+++ rocket_configs.R
@@ -0,0 +1,29 @@
+# Rockets
+estes_hiflier = list(name = "ESTES HI-FLIER",
+                     wt = 31.2, 
+                     mass = 31.2 / 9.8,
+                     rcvr_sys = "STREAMER")
+
+# Motors
+estes_a83 = list(name = "ESTES A8-3",
+                 casing_wt = 15.6,
+                 casing_mass = 15.6 / 9.8,
+                 prop_wt = 0.00875,
+                 mass = 0.00875 / 9.8,
+                 gross_wt = 15.6 + 0.00875,
+                 gross_mass = (15.6 + 0.00875) / 9.8,
+                 impulse = 2.5,
+                 thrust = c(0.0, 5.0, 9.5, 3.0, 2.5, 2.5, 2.5, 0.5, 0.0, 0.0),
+                 t_burn = 0.8,
+                 t_eject = 6.5)
+
+# Part Names, in order of appearance
+rocket_names <- c("estes_hiflier")
+rockets <- list(estes_hiflier)
+names(rockets) <- rocket_names
+
+motor_names <- c("estes_a83")
+motors <- list(estes_a83)
+names(motors) <- motor_names
+
+model_config <- list(rockets = rockets, motors = motors)
