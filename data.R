07fc0993967a796f385a41df3a7ec52687658b4b
Ashton Hansen <ashhansen6@outlook.com>
2023-07-06 20:54:15 -0500

    Started working on data frames to address issues of not being able to do math with lists, added text document describing steps of launch and equations used during different phases of launch.

--- data.R
+++ data.R
@@ -1,11 +1,11 @@
 # Rockets
-estes_hiflier = list(name = "ESTES HI-FLIER",
-                     wt = 31.2, 
+estes_hiflier = c(name = "ESTES HI-FLIER",
+                     wt = (31.2), 
                      mass = 31.2 / 9.8,
                      rcvr_sys = "STREAMER")
 
 # Motors
-estes_a83 = list(name = "ESTES A8-3",
+estes_a83 = c(name = "ESTES A8-3",
                  casing_wt = 15.6, #grams
                  casing_mass = 15.6 / 9.8, #grams
                  prop_wt = 0.00875, #grams
@@ -17,13 +17,6 @@ estes_a83 = list(name = "ESTES A8-3",
                  t_burn = 0.8, #seconds
                  t_eject = 6.5) #seconds
 
-# Part Names, in order of appearance
-rocket_names <- c("estes_hiflier")
-rockets <- list(estes_hiflier)
-names(rockets) <- rocket_names
-
-motor_names <- c("estes_a83")
-motors <- list(estes_a83)
-names(motors) <- motor_names
-
-model_config <- list(rockets = rockets, motors = motors)
+# Data Structures
+rockets = data.frame(estes_hiflier)
+motors = data.frame(estes_a83)
