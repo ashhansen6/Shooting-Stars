90b22412bf3400fb2162d425a5856f89c73e6829
Ashton Hansen <ashhansen6@outlook.com>
2023-08-10 23:17:48 -0400

    Updated calculations for vertical velocity and max height, added pracma package to list

--- data.R
+++ data.R
@@ -3,3 +3,13 @@ rockets <- read.xlsx(workbook_path, sheetName = "Rocket")
 motors <- read.xlsx(workbook_path, sheetName = "Motor")
 thrust <- read.xlsx(workbook_path, sheetName = "Thrust")
 model_lib <- list(rockets = rockets, motors = motors)
+
+# CONFIGURATION:
+## ESTES HI-FLIER W/ ESTES A8-3 MOTOR
+
+thrust_expanded <- thrust %>%
+  filter(brand == "ESTES", name == "A83") %>%
+  select(thrust, time) %>%
+  bind_rows(data.frame(thrust = rep(0, 70), time = seq(3.1, 10, 0.1))) %>% # expanded to capture 10 seconds
+  arrange(time) # Not necessary, keep for redundancy?
+        
--- maths.R
+++ maths.R
@@ -1,6 +1,7 @@
 # To solve for:
-velocity_y = 0 # avg, (m/s); unknown
-velcotiy_x = 0 # avg, (m/s); unknown
+theta = 80 * (pi / 180) # launch angle in degrees converted to radians
+velocity_y = 0 # vector, (m/s); unknown
+velocity_x = 0 # vector, (m/s); unknown
 height = 0 # unknown
 drift = 0 # unknown
 airtime_up = 0 # unknown
@@ -8,9 +9,23 @@ airtime_down = 0 # unknown
 descent_speed = 0 # unknown
 
 ## Max height
+### Vertical Velocity, meters per second (m/s)
+rocket_mass <- rockets$mass[rockets$name == "HI-FLIER"]
+motor_mass <- motors$gross_mass[motors$brand == "ESTES" && motors$name == "A83"]
+combined_mass = (rocket_mass + motor_mass) / 1000 # mass in kilograms (kg)
 
-# <<COMMENT>> Need to calculate average velocity in both x and y direction
-height = sqrt((sin(theta)*velocity_y)^2 + (cos(theta)*velocity_x)^2)
+rocket_wt <- rockets$wt[rockets$name == "HI-FLIER"]
+motor_wt <- motors$gross_wt[motors$brand == "ESTES" && motors$name == "A83"]
+combined_wt = (rocket_wt + motor_wt) / 1000 # weight in kilograms (kg)
+
+force_net_y = (thrust_expanded$thrust - combined_wt) / 10
+accelleration_y = force_net_y / combined_mass
+velocity_y <- cumtrapz(thrust_expanded$time, accelleration_y) * sin(theta)
+position_y <- cumtrapz(thrust_expanded$time, velocity_y)
+height_max <- max(position_y) # NOTE: dragless, need to factor in drag for realism
+
+### Horizontal Velocity
+
 
 ## Descent Speed, feet per tenth of a second 
 descent_speed = (height * full_wt_post) / (drift) %>%
